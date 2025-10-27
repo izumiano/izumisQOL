06df9cc13bbdbd336efefb141687d6ef4ed0406c
izumiano <izumiano@users.noreply.github.com>
2025-10-27 14:47:13 +0100

    add enumerable parser and allow items in arrays and enumerables to have their own parser

--- Scripts/LogParser.cs
+++ Scripts/LogParser.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
 
 namespace Celeste.Mod.izumisQOL.Scripts;
+
 public static class LogParser
 {
 	public static string Default<T>(T obj)
@@ -16,5 +20,20 @@ public static class LogParser
 	public static string Array<T>(T[] array)
 	{
 		return "[" + string.Join(", ", array) + "]";
+	}
+	
+	public static string Array<T>(T[] array, Func<T, string> itemLogParser)
+	{
+		return "[" + string.Join(", ", array.Select(itemLogParser)) + "]";
+	}
+
+	public static string Enumerable<T>(IEnumerable<T> enumerable)
+	{
+		return "[" + string.Join(", ", enumerable) + "]";
+	}
+
+	public static string Enumerable<T>(IEnumerable<T> enumerable, Func<T, string> itemLogParser)
+	{
+		return "[" + string.Join(", ", enumerable.Select(itemLogParser)) + "]";
 	}
 }
